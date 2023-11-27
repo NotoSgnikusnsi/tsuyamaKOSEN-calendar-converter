8c0d222c03ff3ef264e3948b2edb7b472519021f
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-11-27 09:53:28 +0900

    feat: カレンダーを提供するサーバを実装した

---
+++ server.ts
@@ -0,0 +1,38 @@
+import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
+import { CreateCsv, CreateIcal } from "./app.ts";
+
+serve(async (req) => {
+  const pathname = new URL(req.url).pathname;
+  console.log(pathname);
+
+  const year = new Date().getFullYear();
+  let response;
+
+  if (pathname === "/csv" && req.method === "GET") {
+    response = await CreateCsv(year);
+    return new Response(response, {
+      headers: {
+        "content-type": "text/csv",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  }
+
+  if (pathname === "/ical" && req.method === "GET") {
+    const response = await CreateIcal(year);
+    return new Response(response, {
+      headers: {
+        "content-type": "text/calendar",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  }
+
+  return new Response("Not found", {
+    status: 404,
+    headers: {
+      "content-type": "text/plain",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+});
